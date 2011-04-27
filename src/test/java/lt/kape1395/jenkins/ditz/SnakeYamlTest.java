12c8933c06f5819758cee75ff143ff1265466a63
Karolis Petrauskas <k.petrauskas@gmail.com>
2011-04-27 20:50:03 +0300

    Always start from tests.

---
+++ src/test/java/lt/kape1395/jenkins/ditz/SnakeYamlTest.java
@@ -0,0 +1,21 @@
+package lt.kape1395.jenkins.ditz;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class SnakeYamlTest {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+	@Test
+	public void f() {
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXX");
+	}
+
+}
